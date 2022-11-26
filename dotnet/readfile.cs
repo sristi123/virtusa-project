2387d2a5accaf778fe5e4ed6e611191101ffdefb
sristi agrawal <78912160+sristi123@users.noreply.github.com>
2022-11-26 16:54:24 +0530

    Add files via upload

---
+++ dotnet/readfile.cs
@@ -0,0 +1,19 @@
+// using System;
+
+// //We need to include this namespace for file handling.
+// using System.IO;
+
+// namespace ConsoleApplication1
+// {
+//     class Program
+//     {
+//         static void Main()
+//         {
+//             string s;
+
+//             s =File.ReadAllText("ABC.TXT");
+
+//             Console.WriteLine("Content of file :\n"+s);
+//         }
+//     }
+// }
